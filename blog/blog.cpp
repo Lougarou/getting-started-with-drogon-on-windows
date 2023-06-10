2023186813940f62150c4d3c5ddbf1509b7065e4
Lokhesh Ujhoodha <yogisawesome@gmail.com>
2023-06-10 20:56:09 +0200

    basic example

---
+++ blog/blog.cpp
@@ -0,0 +1,17 @@
+﻿// blog.cpp : Defines the entry point for the application.
+//
+
+#include "blog.h"
+#include <drogon/drogon.h>
+
+
+//using namespace std;
+using namespace drogon;
+
+int main()
+{
+    app().setLogPath("./")
+        .addListener("0.0.0.0", 1234)
+        .run();
+	return 0;
+}
